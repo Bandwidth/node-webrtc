e274ecc039779277585d47182d4d42b77402c615
jmulford-bandwidth <jmulford@bandwidth.com>
2021-05-07 14:10:47 -0400

    Added transfer function

--- src/controllers/apiController.ts
+++ src/controllers/apiController.ts
@@ -321,4 +321,34 @@ export class ApiController extends BaseController {
     req.throwOn(404, ApiError, 'Not Found');
     return req.call(requestOptions);
   }
+
+  /**
+   * Generates BXML to transfer a call into a WebRTC session as a complete response, including an XML declaration
+   * header.
+   *
+   * @param {string} deviceToken Token returned from createParticipant call
+   * @param {string} [sipUri=sip:sipx.webrtc.bandwidth.com:5060] SIP URL to transfer to
+   *
+   * @returns BXML string
+   */
+  static generateTransferBxml(deviceToken: string, sipUri='sip:sipx.webrtc.bandwidth.com:5060'): string {
+    return '<?xml version="1.0" encoding="UTF-8" ?>\n'
+        + '<Response>'
+        + this.generateTransferBxmlVerb(deviceToken, sipUri)
+        + '</Response>';
+  }
+
+  /**
+   * Generates the BXML verb to transfer a call into a WebRTC session (not wrapped in a Response element).
+   *
+   * @param {string} deviceToken Token returned from createParticipant call
+   * @param {string} [sipUri=sip:sipx.webrtc.bandwidth.com:5060] SIP URL to transfer to
+   *
+   * @returns BXML string
+   */
+  static generateTransferBxmlVerb(deviceToken: string, sipUri = 'sip:sipx.webrtc.bandwidth.com:5060'): string {
+    return '<Transfer>\n'
+        + `\t<SipUri uui="${deviceToken};encoding=jwt">${sipUri}</SipUri>\n`
+        + '</Transfer>';
+  }
 }
--- tests/api.test.js
+++ tests/api.test.js
@@ -33,4 +33,12 @@ describe('api', () => {
 
         controller.addParticipantToSession(accountId, sessionId, participantId);
     });
+
+    it('should generate transfer bxml', () => {
+        var bxmlString = ApiController.generateTransferBxml("test-string");
+        bxmlString = bxmlString.replace(/\s/g, "");
+
+        var expectedString = '<?xmlversion="1.0"encoding="UTF-8"?><Response><Transfer><SipUriuui="test-string;encoding=jwt">sip:sipx.webrtc.bandwidth.com:5060</SipUri></Transfer></Response>';
+        expect(bxmlString).toEqual(expectedString);
+    });
 });
