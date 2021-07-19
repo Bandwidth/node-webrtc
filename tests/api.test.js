14b7396125d326ce8357f0acb8829b430f28b19e
Audrey Copeland <acopeland@bandwidth.com>
2021-07-19 16:20:44 -0400

    Fix tests

--- tests/api.test.js
+++ tests/api.test.js
@@ -35,10 +35,10 @@ describe('api', () => {
     });
 
     it('should generate transfer bxml', () => {
-        var bxmlString = ApiController.generateTransferBxml("test-string");
+        var bxmlString = ApiController.generateTransferBxml("test-string", "c-93d6f3c0-be584596-0b74-4fa2-8015-d8ede84bd1a4");
         bxmlString = bxmlString.replace(/\s/g, "");
 
-        var expectedString = '<?xmlversion="1.0"encoding="UTF-8"?><Response><Transfer><SipUriuui="test-string;encoding=jwt">sip:sipx.webrtc.bandwidth.com:5060</SipUri></Transfer></Response>';
+        var expectedString = '<?xmlversion="1.0"encoding="UTF-8"?><Response><Transfer><SipUri>uui="93d6f3c0be5845960b744fa28015d8ede84bd1a4;encoding=base64,test-string;encoding=jwt">sip:sipx.webrtc.bandwidth.com:5060</SipUri></Transfer></Response>';
         expect(bxmlString).toEqual(expectedString);
     });
 });
